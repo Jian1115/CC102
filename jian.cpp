7d907e6b0f9a846a7a5cd82dd51b63388c402229
Jian Kim Alto <jianalto24@gmail.com>
2024-12-17 15:25:05 +0800

    Add files via upload

--- jian.cpp
+++ jian.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-    int rows = 6; 
-    char evenChars[] = {'@', '#'}; 
-    char oddChars[] = {'#', '$'};  
-    for (int i = 0; i < rows; i++) {
-       
-        for (int j = 0; j < i; j++) {
-            cout << "  ";
-        }
-
-        
-        if (i % 2 == 0) { 
-            for (int j = 0; j < rows - i; j++) {
-                cout << evenChars[j % 2] << " ";
-            }
-        } else { 
-            for (int j = 0; j < rows - i; j++) {
-                cout << oddChars[j % 2] << " ";
-            }
-        }
-        cout << endl; 
-    }
-
-    return 0;
+int main(){
+	cout << "Revised Pattern 2" << endl;
+	
+	for (int i=0; i<7; i++){
+		for(int j=0; j<i; j++){
+			cout << "  ";
+		}
+		for(int j=i; j<7; j++){
+			if((i % 2 == 0 && j % 2 == 1 || i % 2 == 1 && j % 2 == 1 )){
+				cout << "# ";
+			}
+			else if ((i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 1 )){
+				cout << "@ ";
+			}
+			else{
+				cout << "$ ";
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+	
 }
